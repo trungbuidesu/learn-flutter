ca82405f6416e5b938f5ead6dc83f6f538cf5e64
afishlover <trungbuiducbuiduc@gmail.com>
2025-01-21 21:47:54 +0700

    Update expense_create.dart

--- expenses_tracker/lib/widgets/expense_create.dart
+++ expenses_tracker/lib/widgets/expense_create.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ExpenseCreate extends StatefulWidget {
   const ExpenseCreate({super.key});
@@ -13,15 +14,31 @@ class _ExpenseCreateState extends State<ExpenseCreate> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _amountController = TextEditingController();
 
-  void _showDatePicker() {
+  DateTime? selectedDate;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _showDatePicker() async {
     final currentDate = DateTime.now();
     final firstDate =
         DateTime(currentDate.year - 1, currentDate.month, currentDate.day);
-    showDatePicker(
+    final pickedDate = await showDatePicker(
         context: context,
-        initialDate: currentDate,
+        initialDate: selectedDate ?? currentDate,
         firstDate: firstDate,
         lastDate: currentDate);
+    setState(() {
+      selectedDate = pickedDate;
+    });
+  }
+
+  void _cancelCreateExpense() {
+    Navigator.pop(context);
   }
 
   @override
@@ -31,27 +48,64 @@ class _ExpenseCreateState extends State<ExpenseCreate> {
       child: Column(
         children: [
           TextField(
+            maxLength: 50,
             controller: _titleController,
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
               label: Text("Title"),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              label: Text("Amount"),
-              prefixText: "\$ ",
-            ),
+          const SizedBox(
+            height: 16,
           ),
-          IconButton(
-              onPressed: _showDatePicker, icon: Icon(Icons.calendar_month)),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {});
-              },
-              child: Text("Press me"))
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text("Amount"),
+                    prefixText: "\$ ",
+                  ),
+                ),
+              ),
+              ElevatedButton.icon(
+                  label: Text(
+                    selectedDate == null
+                        ? "Select Date"
+                        : DateFormat("dd/MM/yyyy").format(selectedDate!),
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: _showDatePicker,
+                  icon: Icon(Icons.calendar_month)),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 10,
+            children: [
+              ElevatedButton(
+                  onPressed: _cancelCreateExpense,
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text(
+                    "Save expense",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ))
+            ],
+          ),
         ],
       ),
     );
